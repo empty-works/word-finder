3a6dd2e65dc72028b3751237059f9b7dac50697b
empty-works <pama.mark@gmail.com>
2020-05-10 05:08:47 +0200

    Added a function to open the text file.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 
+void open_file(std::ifstream &);
+
 int main() {
 	
 	std::string user_word {};
@@ -9,7 +11,15 @@ int main() {
 	std::cout << "Search for: ";
 	std::getline(std::cin, user_word);
 
-	std::cout << "What you chose: " << user_word << std::endl;
+	std::ifstream in_file;
+	std::string line;
+	open_file(in_file);
 
 	return 0;
+}
+
+void open_file(std::ifstream &in_file) {
+
+	in_file.open("romeojuliet.txt");
+
 }
