c71013718f3243a8cd7ce0574e87e67ee2be01e9
empty-works <pama.mark@gmail.com>
2020-05-11 03:48:04 +0200

    Implemented ParseLine in LineReader.

--- line_reader.cpp
+++ line_reader.cpp
@@ -8,5 +8,15 @@ void LineReader::ReadLines(std::ifstream &in_file) {
 }
 
 std::vector<std::string> LineReader::ParseLine(const std::string &line) {
-	
+	std::vector<std::string> words_vec{};
+	std::size_t current, previous = 0;
+	char delimiter{' '};
+	current = line.find(delimiter);
+	while(current != std::string::npos) {
+		words_vec.push_back(line.substr(previous, current - previous));
+		previous = current + 1;
+		current = line.find(delimiter, previous);	
+	}
+	words_vec.push_back(line.substr(previous, current - previous));
+	return words_vec;	
 }
